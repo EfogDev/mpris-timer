458056d3548010cb152b56b07c082005c5171eca
Artem Suprun <efogdev@gmail.com>
2024-11-06 04:43:37 +0200

    chore: refactoring

--- internal/util/cli.go
+++ internal/util/cli.go
@@ -18,11 +18,11 @@ var (
 func LoadFlags() {
 	flag.BoolVar(&Notify, "notify", UserPrefs.EnableNotification, "Send desktop notification")
 	flag.BoolVar(&Sound, "sound", UserPrefs.EnableSound, "Play sound")
-	flag.Float64Var(&Volume, "volume", UserPrefs.Volume, "Volume (0-1)")
+	flag.Float64Var(&Volume, "volume", UserPrefs.Volume, "Volume [0-1]")
 	flag.BoolVar(&UseUI, "ui", false, "Show timepicker UI (default true)")
 	flag.IntVar(&Duration, "start", 0, "Start the timer immediately")
 	flag.StringVar(&Title, "title", UserPrefs.DefaultTitle, "Name/title of the timer")
 	flag.StringVar(&Text, "text", UserPrefs.DefaultText, "Notification text")
-	flag.StringVar(&Color, "color", UserPrefs.ProgressColor, "Progress color in the player, rgb hex value, i.e. #000000")
+	flag.StringVar(&Color, "color", UserPrefs.ProgressColor, "Progress color for the player")
 	flag.Parse()
 }
